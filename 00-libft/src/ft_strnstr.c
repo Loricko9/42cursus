0ce47209530cb91a76f89e28328ab55926a224ac
Loricko9 <lesaulnierlorick@gmail.com>
2023-08-28 21:21:53 +0200

    ajout strncmp & strnstr

---
+++ 00-libft/src/ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lle-saul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 18:49:39 by lle-saul          #+#    #+#             */
+/*   Updated: 2023/08/28 18:49:39 by lle-saul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!little)
+		return ((char *)big);
+	while (big[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (big[i] == little[j] && i < len)
+		{
+			if (little[j + 1] == '\0')
+				return ((char *)&big[i - j]);
+			i++;
+			j++;
+		}
+		i = (i + 1) - j;
+	}
+	return (0);
+}
