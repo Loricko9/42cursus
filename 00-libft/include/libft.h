e3e014549ae7c212415de1259b948b441cbd849d
Loricko9 <lesaulnierlorick@gmail.com>
2023-09-08 14:50:56 +0200

    memcpy

--- 00-libft/include/libft.h
+++ 00-libft/include/libft.h
@@ -25,6 +25,7 @@ int	ft_atoi (const char *nptr);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strdup(const char *s);
 char	*strchr(const char *s, int c);
 char	*strrchr(const char *s, int c);
---
+++ 00-libft/src/ft_memcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lle-saul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 08:32:04 by lle-saul          #+#    #+#             */
+/*   Updated: 2023/09/04 08:32:04 by lle-saul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dest && !src)
+		return (0);
+	while (i < n)
+	{
+		((unsigned char*)dest)[i] == ((unsigned char*)src)[i];
+		i++;
+	}
+	return (dest);
+}
