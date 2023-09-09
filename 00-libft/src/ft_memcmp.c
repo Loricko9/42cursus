cd9d248dff32d0f9c31f3d1044ff3e9f34f4ca3d
Loricko9 <lesaulnierlorick@gmail.com>
2023-09-09 16:51:46 +0200

    memcmp

---
+++ 00-libft/src/ft_memcmp.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lle-saul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 16:32:28 by lle-saul          #+#    #+#             */
+/*   Updated: 2023/09/09 16:32:28 by lle-saul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char*)s1)[i] != ((unsigned char*)s1)[i])
+			return (((unsigned char*)s1)[i] - ((unsigned char*)s1)[i]);
+		i++;
+	}
+	return (0);
+}
