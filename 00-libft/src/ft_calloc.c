8cf7a6eea755a21eca321ea0fa96c289ffa34f08
Loricko9 <lesaulnierlorick@gmail.com>
2023-09-11 17:20:29 +0200

    calloc

---
+++ 00-libft/src/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lle-saul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 16:31:23 by lle-saul          #+#    #+#             */
+/*   Updated: 2023/09/11 16:31:23 by lle-saul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+#include <stdlib.h>
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*tab;
+
+	tab = malloc(nmemb * size);
+	if (!tab)
+		return (NULL);
+	ft_bzero(tab, nmemb);
+	return (tab);
+}
