6a523bb8b3ed59a4a8913341f5359abccc204ade
Loricko9 <lesaulnierlorick@gmail.com>
2023-09-22 16:20:05 +0200

    strtrim

---
+++ 00-libft/src/ft_strtrim.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lle-saul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 15:50:56 by lle-saul          #+#    #+#             */
+/*   Updated: 2023/09/22 15:50:56 by lle-saul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+#include <libft.h>
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	i;
+	char	*str;
+
+	if (!s1 || !set)
+		return (0);
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] != '\0' && ft_strchr(set, s1[start]) != 0)
+		start++;
+	while (end != 0 && ft_strchr(set, s1[end]) != 0)
+		end++;
+	str = malloc(sizeof(char) * (end - start + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (start <= end)
+	{
+		str[i] = s1[start];
+		start++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
