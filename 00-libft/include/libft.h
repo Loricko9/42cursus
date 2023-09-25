c49b907b1b15bea3e9d5387ef351d5002377af35
Loricko9 <lesaulnierlorick@gmail.com>
2023-09-25 11:15:42 +0200

    lstadd_back

--- 00-libft/include/libft.h
+++ 00-libft/include/libft.h
@@ -23,6 +23,7 @@ struct s_list	*next;
 t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 int		ft_isprint(int c);
 int		ft_isascii(int c);
---
+++ 00-libft/src/ft_lstadd_back.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lle-saul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 11:03:27 by lle-saul          #+#    #+#             */
+/*   Updated: 2023/09/25 11:03:27 by lle-saul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (new)
+	{
+		if (!*lst)
+		{
+			*lst = new;
+			return ;
+		}
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+}
