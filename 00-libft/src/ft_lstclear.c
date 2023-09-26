e9e455932d87c0b7450fc9ea6d6d8d88daf5ae62
Loricko9 <lesaulnierlorick@gmail.com>
2023-09-26 11:15:01 +0200

    lstclear & lstdelone

---
+++ 00-libft/src/ft_lstclear.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lle-saul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 10:56:33 by lle-saul          #+#    #+#             */
+/*   Updated: 2023/09/26 10:56:33 by lle-saul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*temp;
+
+	if (*lst)
+	{
+		while ((*lst)->next != NULL)
+		{
+			temp = (*lst)->next;
+			ft_lstdelone(*lst, del);
+			*lst = temp;
+		}
+		*lst = NULL;
+	}	
+}
