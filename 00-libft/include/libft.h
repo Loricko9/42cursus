c5571c1cccb914754f1b045123e5aa7492624459
Loricko9 <lesaulnierlorick@gmail.com>
2023-09-27 11:30:11 +0200

    modif nom bonus & norme header

--- 00-libft/include/libft.h
+++ 00-libft/include/libft.h
@@ -16,12 +16,13 @@
 
 typedef struct s_list
 {
-void			*content;
-struct s_list	*next;
+	void			*content;
+	struct s_list	*next;
 }				t_list;
 
 t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
