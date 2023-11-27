ebde5fac75731c22d8122daed9522b732582e96e
Loricko9 <lesaulnierlorick@gmail.com>
2023-11-27 13:16:43 +0100

    update git

--- 01-printf/main.c
+++
@@ -1,8 +0,0 @@
-#include "ft_printf.h"
-#include <stdio.h>
-
-int	main(void)
-{
-	printf("%d", ft_printf("salut\n"));
-	printf("%d", printf("salut\n"));
-}
--- 01-printf/src/ft_printf.c
+++ 01-printf/src/ft_printf.c
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lle-saul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lle-saul <lle-saul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/10 18:00:53 by lle-saul          #+#    #+#             */
-/*   Updated: 2023/10/10 18:00:53 by lle-saul         ###   ########.fr       */
+/*   Created: 2023/10/10 18:12:50 by lle-saul          #+#    #+#             */
+/*   Updated: 2023/10/10 18:12:50 by lle-saul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	ft_putnbr_unsigned(unsigned int nb, int *count)
+{
+	unsigned long	nbr;
+
+	nbr = nb;
+	if (nbr > 9)
+		ft_putnbr_unsigned(nbr / 10, count);
+	ft_putchar(nbr % 10 + 48, count);
+}
 
 void	ft_printpoint(unsigned long long nb, int *count)
 {
