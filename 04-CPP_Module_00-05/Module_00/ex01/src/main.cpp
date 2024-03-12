c4d2bc8220e265545e0cf831f213688b554a2cb9
Lorick <lo@DESKTOP-IUB6M75.>
2024-03-12 19:38:34 +0100

    add ex01 Module-00

---
+++ 04-CPP_Module_00-05/Module_00/ex01/src/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lle-saul <lle-saul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 16:34:03 by lle-saul          #+#    #+#             */
+/*   Updated: 2024/03/12 17:30:25 by lle-saul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <limits>
+#include "PhoneBook.hpp"
+
+int main(void)
+{
+	PhoneBook	Book;
+	while (1)
+	{
+		std::string str;
+		std::cout << "PhoneBook> " << std::flush;
+		std::cin >> str;
+		if (str == "EXIT")
+			break ;
+		else if (str == "ADD")
+			Book.AddContact();
+		else if (str == "SEARCH")
+			Book.SearchContact();
+	}
+	std::cout << "Fin du prog !" << std::endl;
+}
