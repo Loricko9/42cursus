e1b64b0192aa2901174a55ebabd72907e9b41837
Lorick <lo@DESKTOP-IUB6M75.>
2024-03-17 12:33:01 +0100

    Add ex02 Module_04

---
+++ 04-CPP_Module_00-05/Module_04/ex02/src/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lle-saul <lle-saul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/16 12:30:09 by lle-saul          #+#    #+#             */
+/*   Updated: 2024/03/17 12:12:07 by lle-saul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AAnimal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "Brain.hpp"
+
+int	main()
+{
+	Dog		dog_test;
+	Cat		cat_test;
+	dog_test.addIdeas("eat a steack");
+	cat_test.addIdeas("eat a fish");
+	dog_test.think();
+	cat_test.think();
+	Dog		dog_res(dog_test);
+	Cat		cat_res(cat_test);
+	dog_res.think();
+	cat_res.think();
+	
+	std::cout << "-----------------------------------------------------" << std::endl;
+	
+	AAnimal	*tab[4] = {new Dog(), new Dog(), new Cat(), new Cat()};
+	tab[0]->addIdeas("eat a steack");
+	tab[2]->addIdeas("eat a fish");
+	tab[0]->think();
+	tab[2]->think();
+	
+	for (int i = 0; i < 4; i++)
+		delete tab[i];
+	std::cout << "-----------------------------------------------------" << std::endl;
+}
