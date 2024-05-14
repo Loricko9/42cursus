b8201469de1f5796be66e10fbfb13f94b0a47077
Loricko9 <lesaulnierlorick@gmail.com>
2024-05-14 18:40:37 +0200

    Work

--- 05-Inception/srcs/requirements/mariadb/tools/setup.sh
+++ 05-Inception/srcs/requirements/mariadb/tools/setup.sh
@@ -5,13 +5,16 @@
 #MYSQL_USER=wordpress
 #MYSQL_PASSWORD=wordpress
 
-service mysql start
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > setup.sql
-echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" >> setup.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> setup.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" >> setup.sql
-mysql -p$MYSQL_ROOT_PASSWORD < setup.sql
+if [ ! -f "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+	service mysql start
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > setup.sql
+	echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" >> setup.sql
+	echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> setup.sql
+	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" >> setup.sql
+	mysql -p$MYSQL_ROOT_PASSWORD < setup.sql
+	
+	kill $(cat /var/run/mysqld/mysqld.pid)
+fi
 
 exec mysqld_safe
--- 05-Inception/srcs/requirements/wordpress/tools/setup_wp.sh
+++ 05-Inception/srcs/requirements/wordpress/tools/setup_wp.sh
@@ -11,11 +11,17 @@ if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
 	sed -i "s/votre_utilisateur_de_bdd/$WORDPRESS_DB_USER/" /var/www/wordpress/wp-config-sample.php
 	sed -i "s/votre_mdp_de_bdd/$WORDPRESS_DB_PASSWORD/" /var/www/wordpress/wp-config-sample.php
 	sed -i "s/localhost/$WORDPRESS_DB_HOST/" /var/www/wordpress/wp-config-sample.php
+
 	# rename file for apply change
 	mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 
 	# add data for 2nd page of config
-	wp core install --allow-root --path=/var/www/wordpress --title='lle-saul_WEB' --admin_email='lle-saul@42-perpignan.fr' --admin_user='$WORDPRESS_ADMIN' --admin_password='$WORDPRESS_ADMIN_PASS' --url=localhost
+	wp core install --allow-root --path=/var/www/wordpress --title='lle-saul_WEB' --admin_email='lle-saul@42-perpignan.fr' --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASS --url=localhost
+
+	wp theme install vertice --path=/var/www/wordpress --activate --allow-root
+
+ 	# create 2nd user
+ 	wp user create --allow-root --path=/var/www/wordpress $WORDPRESS_USER test@test.fr --user_pass=$WORDPRESS_USER_PASS --role=subscriber
 fi
 
 if [ ! -f "/run/php" ]; then
